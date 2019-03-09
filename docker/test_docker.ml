4b80a24275f7993d9f33e32c77865913db15f375
Marcello Seri <marcello.seri@gmail.com>
2019-03-09 00:15:43 +0000

    travis: test txt, not plot
    Alpine does not have plplot
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- docker/test_docker.ml
+++ docker/test_docker.ml
@@ -4,29 +4,17 @@ exec ocaml "$0" "$@"
 *) use "topfind";;
 
 #require "owl";;
-#require "owl-plplot";;
 
 open Owl
-open Owl_plplot
 
 let () =
   let fname =
     if Array.length Sys.argv < 2 then
-      "plot.png"
+      "test.txt"
     else
-      Sys.argv.(1) ^ ".png"
+      Sys.argv.(1) ^ ".txt"
   in
   let f x = Maths.sin x /. x in
-  let h = Plot.create fname in
-
-  Plot.set_foreground_color h 0 0 0;
-  Plot.set_background_color h 255 255 255;
-  Plot.set_title h "Function: f(x) = sine x / x";
-  Plot.set_xlabel h "x-axis";
-  Plot.set_ylabel h "y-axis";
-  Plot.set_font_size h 8.;
-  Plot.set_pen_size h 3.;
-  Plot.plot_fun ~h f 1. 15.;
-
-  Plot.output h;;
+  let t = Mat.linspace 0. Owl.Const.pi 10 in  
+  Mat.save_txt (Arr.map f t) fname
 
